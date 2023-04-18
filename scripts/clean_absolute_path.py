7c4d0013aa06bfd818e7e5085295ba0234bc1d3c
tobiasklein <kle.tobias@googlemail.com>
2023-04-18 14:34:49 +0200

    Updates to script not working still

--- scripts/clean_absolute_path.py
+++ scripts/clean_absolute_path.py
@@ -8,41 +8,41 @@ dirs= {
     }
 
 # The pattern to search for
-pattern = r'^(.*?)([^/]+/?)$'
+pattern = r'("/Users/tobias/(?:[^/]+/)*)[^/]+/?(?:"$|$)'
 
 # A regular expression object for the pattern
-regex = re.compile(pattern)
+regex = re.compile(pattern,re.I)
 
 # The string to replace the pattern with
-replacement_base = "/path/to/destination/"
+replacement_base = "/path//to//destination//"
 
-for i in [os.path.join(dirs['base'].value(),dirs[k].value()) for k in dirs.keys() if k not "base"]:
+for i in [os.path.join(dirs['base'],dirs[k]) for k in dirs.keys() if k != "base"]:
     os.chdir(i)
+    print(f'os.getcwd() \n {os.getcwd()} \n')
     # Walk through all directories and subdirectories
     for root, dirs, files in os.walk("."):
         # Check if any of the files are markdown files
         for file in files:
-            if file.endswith(".md"):
-                # Construct the full path to the file
-                filepath = os.path.join(root, file)
-                # Open the file and read its contents
-                with open(filepath, "r") as f:
-                    contents = f.read()
-                # Split the contents into lines
-                lines = contents.split('\n')
-                # Substitute the pattern with the replacement string for each line
-                new_lines = []
-                for line in lines:
-                    match = regex.match(line)
-                    if match:
-                        excluded_part = match.group(2)
-                        replacement = replacement_base + excluded_part
-                        new_line = regex.sub(replacement, line)
+                if file.endswith(".md"):
+                    # Construct the full path to the file
+                    filepath = os.path.join(root, file)
+                    print(f'filepath \n {filepath} \n')
+                    # Open the file and read its contents
+                    with open(filepath, "r") as f:
+                        contents = f.read()
+                    # Split the contents into lines
+                    lines = contents.split('\n')
+                    # Substitute the pattern with the replacement string for each line
+                    new_lines = []
+                    for line in lines:
+                        new_line = line
+                        for match in regex.finditer(line):
+                            excluded_part = match.group(1)
+                            replacement = replacement_base + excluded_part
+                            new_line = new_line.replace(match.group(0), replacement)
                         new_lines.append(new_line)
-                    else:
-                        new_lines.append(line)
-                # Join the lines back into a single string
-                new_contents = '\n'.join(new_lines)
-                # Write the new contents back to the file
-                with open(filepath, "w") as f:
-                    f.write(new_contents)
+                    # Join the lines back into a single string
+                    new_contents = '\n'.join(new_lines)
+                    # Write the new contents back to the file
+                    with open(filepath, "w") as f:
+                        f.write(new_contents)
