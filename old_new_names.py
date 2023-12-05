4a142d9ce7944967c34ac010a65bd930a363c381
Tobias Klein <kle.tobias@googlemail.com>
2023-12-05 19:43:33 +0100

    refactor(old_new_names.py): Removed the commented lines that are no longer part of the file.

--- old_new_names.py
+++ old_new_names.py
@@ -45,30 +45,4 @@ def name_pairs(original: str, updated: str) -> Dict[str, str]:
     return(dict(zip(original_h3, updated_h3)))
 
 
-# %%
-# def name_pairs(original: str, updated: str) -> Dict:
-#     """
-#     Get old and new file names and create dictionary from them.
 
-#     Args:
-#     original (str): File path to the file with the original filenames.
-
-#     updated (str): File path to the file with the updated filenames.
-
-#     Returns:
-#      Dictionary: keys are original names and values are updated names.
-#     """
-#     h3_pattern = re.compile('^(###\s.+)')
-#     original_h3 = []
-#     updated_h3 = []
-
-
-#     with open(original, 'r') as f:
-#         original_names = f.readlines()
-
-#     with open(updated, 'r') as f:
-#         updated_names = f.readlines()
-
-#     for old_line,new_line in zip(original_names,updated_names):
-#         if h3_pattern.search(old_line) != None:
-#             match_old = h3_pattern.search(old_line)[0]
