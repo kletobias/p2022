6452dcd3a7662161c9a8842f624c60d4e8c9bd48
Tobias Klein <kle.tobias@googlemail.com>
2023-12-05 19:37:42 +0100

    feat(old_new_names.py): add read_lines_with_pattern function.

--- old_new_names.py
+++ old_new_names.py
@@ -1,32 +1,51 @@
 import os
 import re
-from typing import Dict
+from typing import Dict, Optional, Tuple
 
-def name_pairs(original: str, updated: str) -> Dict:
+
+def read_lines_with_pattern(filename: str, pattern: re.Pattern) -> Optional:
     """
-    Get old and new file names and create dictionary from them.
+    Read lines from a file and extract matching patterns.
 
     Args:
-    original (str): File path to the file with the original filenames.
-
-    updated (str): File path to the file with the updated filenames.
+    filename (str): File path to read.
 
     Returns:
-     Dictionary: keys are original names and values are updated names.
+    Optional[Tuple[str]]: Tuple of matched strings, or None if the file can't be opened.
     """
-    h3_pattern = re.compile('^(###\s.+)')
-    original_h3 = []
-    updated_h3 = []
+    try:
+        with open(filename, "r") as file:
+            return tuple(
+                pattern.search(line)[0] for line in file if pattern.search(line)
+            )
+    except OSError as e:
+        print(f"Error reading file {filename}: {e}")
+        return None
 
 
-    with open(original, 'r') as f:
-        original_names = f.readlines()
+# def name_pairs(original: str, updated: str) -> Dict:
+#     """
+#     Get old and new file names and create dictionary from them.
 
-    with open(updated, 'r') as f:
-        updated_names = f.readlines()
+#     Args:
+#     original (str): File path to the file with the original filenames.
 
-    for old_line,new_line in zip(original_names,updated_names):
-        if h3_pattern.search(old_line) != None:
-            match_old = h3_pattern.search(old_line)[0]
+#     updated (str): File path to the file with the updated filenames.
+
+#     Returns:
+#      Dictionary: keys are original names and values are updated names.
+#     """
+#     h3_pattern = re.compile('^(###\s.+)')
+#     original_h3 = []
+#     updated_h3 = []
 
 
+#     with open(original, 'r') as f:
+#         original_names = f.readlines()
+
+#     with open(updated, 'r') as f:
+#         updated_names = f.readlines()
+
+#     for old_line,new_line in zip(original_names,updated_names):
+#         if h3_pattern.search(old_line) != None:
+#             match_old = h3_pattern.search(old_line)[0]
