31c3863e88a2685d411f034a87caf8e212ad9596
Tobias Klein <kle.tobias@googlemail.com>
2023-12-07 12:23:15 +0100

    feat(old_new_names.py): add dump dictionary to file as input to update_titles.py script

--- old_new_names.py
+++ old_new_names.py
@@ -1,6 +1,7 @@
 import os
 import re
 from typing import Dict, Optional
+import json
 
 
 def read_lines_with_pattern(filename: str, pattern: re.Pattern) -> Optional:
@@ -23,7 +24,7 @@ def read_lines_with_pattern(filename: str, pattern: re.Pattern) -> Optional:
         return None
 
 
-def name_pairs(original: str, updated: str) -> Dict[str, str]:
+def name_pairs(original: str, updated: str) -> Optional:
     """
     Create a dictionary mapping old filenames to new filenames.
 
@@ -42,15 +43,36 @@ def name_pairs(original: str, updated: str) -> Dict[str, str]:
     if original_h3 is None or updated_h3 is None:
         return {}
 
-
-
     return(dict(zip(original_h3, updated_h3)))
+
+
+def dump_dict_to_json(file_path: str, data: Dict[str, str]) -> None:
+    """
+    Dumps a dictionary to a JSON file.
+
+    Args:
+    file_path (str): The file path where the JSON should be saved.
+    data (Dict[str, str]): The dictionary to be dumped into the file.
+
+    Returns:
+    None
+    """
+    export_dir = os.path.dirname(file_path)
+    if not os.path.exists(export_dir):
+        os.makedirs(export_dir)
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data,file,ensure_ascii=False, indent=4)
+    
+
+
 
 root_dir = "/Users/tobias/all_code/projects/portfolio-website-2022/scripts/update_file_details/"
 original = os.path.join(root_dir,"original.md")
 updated = os.path.join(root_dir,"updated.md")
 
 pairs_dict = name_pairs(original,updated)
+dump_dict_to_json(os.path.join(root_dir,"export_old_new_name_pairs","old_new_names.json"),pairs_dict)
 
 for key, value in pairs_dict.items():
     print('\n')
