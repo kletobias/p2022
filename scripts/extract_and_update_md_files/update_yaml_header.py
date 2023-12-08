0448c92896182c4cfe4f0bd4706f3734e7763e0d
Tobias Klein <kle.tobias@googlemail.com>
2023-12-08 14:20:10 +0100

    feat(update_yaml_header.py): add load_json_from_file function and constant TITLE_MAPPING_JSON_FILE

--- scripts/extract_and_update_md_files/update_yaml_header.py
+++ scripts/extract_and_update_md_files/update_yaml_header.py
@@ -6,6 +6,7 @@ from typing import Dict, Tuple, List
 # Constants
 DIR = "/Users/tobias/all_code/projects/portfolio-website-2022/_projects/"
 EXPORT_DIR = "/Users/tobias/all_code/projects/portfolio-website-2022/scripts/extract_and_update_md_files/export/updated_yaml_header/"
+TITLE_MAPPING_JSON_FILE = "/Users/tobias/all_code/projects/portfolio-website-2022/scripts/extract_and_update_md_files/export/old_new_mapping.json"
 
 def create_export_file_paths(input_dir: str, export_dir: str) -> List[str]:
     input_file_names = [filename for filename in os.listdir(input_dir) if filename.endswith('.md')]
@@ -31,4 +32,25 @@ def write_string_to_file(file_path: str, string_content: str) -> None:
         print(f"Error writing to file: {e}")
 
 # write_string_to_file(file_path='path_to_file.txt', string_content=your_string_variable)
+
+def load_json_from_file(file_path: str) -> dict:
+    """
+    Load JSON data from a file.
+
+    Args:
+    file_path (str): The path to the JSON file to be read.
+
+    Returns:
+    dict: The JSON data loaded from the file.
+    """
+    import json
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"Error loading JSON from file: {e}")
+        return {}
+
+# loaded_data = load_json_from_file(file_path=TITLE_MAPPING_JSON_FILE)
 
