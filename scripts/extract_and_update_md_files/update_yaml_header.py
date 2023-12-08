22edd76128a5b46349386b03adce330dacf677db
Tobias Klein <kle.tobias@googlemail.com>
2023-12-08 13:39:25 +0100

    feat(update_yaml_header.py): add imports and constants

---
+++ scripts/extract_and_update_md_files/update_yaml_header.py
@@ -0,0 +1,8 @@
+import os
+import re
+from typing import Dict, Tuple
+
+
+# Constants
+DIR = "/Users/tobias/all_code/projects/portfolio-website-2022/_projects/"
+EXPORT_DIR = "/Users/tobias/all_code/projects/portfolio-website-2022/scripts/extract_and_update_md_files/export/updated_yaml_header/"
